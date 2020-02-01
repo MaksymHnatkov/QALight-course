1f5ea837f01120bbbf27f62de938a4d4db21ed9a
MaksymHnatkov <57106679+MaksymHnatkov@users.noreply.github.com>
2020-02-01 16:46:47 +0200

    Домашнее задание №3 часть 3. Гнатков

--- dz3p3.py
+++ dz3p3.py
@@ -3,7 +3,7 @@ from random import randint as ri
 userString = (str(input("Введіть будь-яку фразу та натисніть Enter: ")))
 
 def func (x,y,z):
-    result = {x:ri(y,z)}
+    result = {x:ri(y,z+1)}
     return result
 
 print (func(userString,0,999))
